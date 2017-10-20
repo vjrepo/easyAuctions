e4036dbd8608f03fcf23dadab954b961bc32100f
valli <valli@BLRVIKR17.sapient.com>
2017-10-20 16:04:32 +0530

    Moved transactions to spring from javax

--- src/main/java/org/vj/auctions/service/ItemServiceImpl.java
+++ src/main/java/org/vj/auctions/service/ItemServiceImpl.java
@@ -2,10 +2,13 @@ package org.vj.auctions.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.vj.auctions.dao.ItemDao;
 import org.vj.auctions.model.Item;
 
@@ -15,12 +18,12 @@ public class ItemServiceImpl implements ItemService {
 	@Autowired
 	private ItemDao itemDao;
 
-	@Transactional
+	@Transactional(isolation=Isolation.READ_COMMITTED, propagation=Propagation.REQUIRED)
 	public boolean createItem(Item item) {
 		return itemDao.createItem(item);
 	}
 
-	@Transactional
+	@Transactional(isolation=Isolation.READ_COMMITTED, propagation=Propagation.REQUIRED)
 	public List<Item> findAll() {
 		return itemDao.findAll();
 	}
